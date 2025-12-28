e514134f5f171ae96a0c4e24d7c9a15e225d9ee0
ApuKM <apukumar180@gmail.com>
2025-12-29 00:39:10 +0600

    params problem

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import ProductInteraction from "@/components/ProductInteraction";
 import { ProductType } from "@/types";
 import Image from "next/image";
 
+// TEMPORARY
 const product: ProductType = {
   id: 1,
   name: "Adidas CoreFit T-Shirt",
@@ -9,7 +10,7 @@ const product: ProductType = {
     "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
   description:
     "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-  price: 39.9,
+  price: 59.9,
   sizes: ["xs", "s", "m", "l", "xl"],
   colors: ["gray", "purple", "green"],
   images: {
@@ -22,13 +23,11 @@ const product: ProductType = {
 export const generateMetadata = async ({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 }) => {
-  const { id } = await params;
-
   return {
     title: product.name,
-    description: product.description,
+    describe: product.description,
   };
 };
 
@@ -37,16 +36,15 @@ const ProductPage = async ({
   searchParams,
 }: {
   params: Promise<{ id: string }>;
-  searchParams: Promise<{ color?: string; size?: string }>;
+  searchParams: Promise<{ color: string; size: string }>;
 }) => {
-  const { id } = await params;
-  const { color, size } = await searchParams;
+  const { size, color } = await searchParams;
 
-  const selectedColor = color || product.colors[0];
-  const selectedSize = size || product.sizes[0];
-
+  const selectedSize = size || (product.sizes[0] as string);
+  const selectedColor = color || (product.colors[0] as string);
   return (
     <div className="flex flex-col gap-4 lg:flex-row md:gap-12 mt-12">
+      {/* IMAGE */}
       <div className="w-full lg:w-5/12 relative aspect-[2/3]">
         <Image
           src={product.images[selectedColor]}
@@ -55,17 +53,48 @@ const ProductPage = async ({
           className="object-contain rounded-md"
         />
       </div>
-
+      {/* DETAILS */}
       <div className="w-full lg:w-7/12 flex flex-col gap-4">
         <h1 className="text-2xl font-medium">{product.name}</h1>
         <p className="text-gray-500">{product.description}</p>
         <h2 className="text-2xl font-semibold">${product.price.toFixed(2)}</h2>
-
         <ProductInteraction
           product={product}
-          selectedColor={selectedColor}
           selectedSize={selectedSize}
+          selectedColor={selectedColor}
         />
+        {/* CARD INFO */}
+        <div className="flex items-center gap-2 mt-4">
+          <Image
+            src="/klarna.png"
+            alt="klarna"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/cards.png"
+            alt="cards"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/stripe.png"
+            alt="stripe"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+        </div>
+        <p className="text-gray-500 text-xs">
+          By clicking Pay Now, you agree to our{" "}
+          <span className="underline hover:text-black">Terms & Conditions</span>{" "}
+          and <span className="underline hover:text-black">Privacy Policy</span>
+          . You authorize us to charge your selected payment method for the
+          total amount shown. All sales are subject to our return and{" "}
+          <span className="underline hover:text-black">Refund Policies</span>.
+        </p>
       </div>
     </div>
   );
